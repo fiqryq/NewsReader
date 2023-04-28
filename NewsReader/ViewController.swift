db8f7dc11ed101022094e2f3c5effa6115d77169
fiqryq <fiqrychoerudin48@gmail.com>
2023-04-28 09:21:55 +0700

    feat: add login screen & tab bar

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Fiqry Choerudin on 27/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
